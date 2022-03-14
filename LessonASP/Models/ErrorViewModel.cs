c2abd114e412f5872d7b6df1cf99809dca06c199
Max7610 <56383853+Max7610@users.noreply.github.com>
2022-03-14 18:54:35 +0300

    Add files via upload

---
+++ LessonASP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LessonASP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
