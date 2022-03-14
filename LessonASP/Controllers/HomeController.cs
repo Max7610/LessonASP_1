0a03ce83b09b5a2400e4cb5c80367170e10aa0f0
Max7610 <56383853+Max7610@users.noreply.github.com>
2022-03-14 18:52:15 +0300

    Add files via upload

---
+++ LessonASP/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using LessonASP.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LessonASP.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static readonly List<Employee> __Employees = new ()
+        {
+            new Employee { Id = 1, LastName ="Иванов", FirstName="Иван",Patronymic="Иванович",Age=23},
+            new Employee { Id = 2, LastName = "Петров", FirstName = "Пётр", Patronymic = "Петрович", Age = 18 },
+            new Employee { Id = 3, LastName = "Сергеев", FirstName = "Сергей", Patronymic = "Сергеевич", Age = 32 }
+        };
+
+        private readonly IConfiguration _Configuration;
+
+        public HomeController(IConfiguration Configuration)
+        {
+            _Configuration = Configuration;
+        }
+       
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ContentStr(string Id = "-id-")
+        {
+            return Content($"content: {Id}");
+        }
+        public IActionResult ConfigStr()
+        {
+            return Content($"config:{_Configuration["ServerGreetings"]}");
+        }
+      
+        public IActionResult Employees()
+        {
+            return View(__Employees);      
+        }
+       
+    }
+}
---
+++ LessonASP/Controllers/Home_2Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LessonASP.Controllers
+{
+    public class Home_2Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
